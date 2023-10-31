2b00a7f7f6e9fd03649900aeb2b7da498769710b
btrungnguyen <121739440+btrungnguyen@users.noreply.github.com>
2023-10-31 12:07:23 -0500

    First_commit
    Date 31-10-2023

---
+++ fact_internetsale_prepared.sql
@@ -0,0 +1,15 @@
+-- prepapred FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  [SalesOrderNumber], 
+  [SalesAmount] 
+FROM 
+  dbo.FactInternetSales
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 
+ORDER BY
+  OrderDateKey ASC
